dd5fd441b7716d25010107cb4961d9673a24b9ae
sathley <sathley@appacitive.com>
2013-12-05 06:07:10 +0530

    added basic project skeleton

---
+++ PyAppacitive/settings.py
@@ -0,0 +1,27 @@
+__author__ = 'sushant'
+
+
+# primary settings
+api_key = "< enter your api key here >"
+environment = "< sandbox or live >"
+api_base_url = "https://apis.appacitive.com/"
+
+# email settings
+email_config = {
+    "smtp": {
+        "username": None,
+        "password": None,
+        "host": "smtp.gmail.com",
+        "port": 465,
+        "enable-ssl": True
+    },
+    "reply-to": None,
+    "from": None}
+
+# push settings
+
+
+
+
+
+
---
+++ PyAppacitive/src/http.py
@@ -0,0 +1,21 @@
+__author__ = 'sushant'
+
+
+
+
+'''
+
+def put(url, payload):
+    pass
+
+
+def post(url, payload):
+    pass
+
+
+def delete(url):
+    pass
+
+if __name__ == "__main__":
+    print get("http://www.google.com")
+    '''
