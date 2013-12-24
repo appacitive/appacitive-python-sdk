164c4c20f0659506e3b948246db7974ddfd2fd3d
sathley <sathley@appacitive.com>
2013-12-24 20:01:01 +0530

    added email templated test and coverage config file

--- tests/email_test.py
+++ tests/email_test.py
@@ -18,3 +18,12 @@ def send_email_test_without_config():
     response = AppacitiveEmail.send_raw_email(['sathley@appacitive.com'], 'hello from py sdk', 'Wazza!', from_email='sathley@appacitive.com')
 
     assert response.status.code == '200'
+
+
+def send_templated_email_test():
+    response = AppacitiveEmail.send_templated_email(['sathley@appacitive.com'], 'Hello from py sdk', 'sample', {
+        'username': 'john.doe',
+        'firstname': 'John',
+        'lastname': 'Doe'
+    },[], [], from_email='sathley@appacitive.com' )
+    assert response.status.code == '200'
